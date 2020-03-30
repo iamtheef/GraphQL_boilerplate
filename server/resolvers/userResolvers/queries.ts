c319d131ed25fd2bbc4042b81b5cd723704e1c4d
Eftihis Thergiakis <eftihisthergiakis@Eftihiss-MacBook-Pro.local>
2020-03-30 15:00:29 +0300

    user query resolver

--- server/resolvers/userResolvers/queries.ts
+++ server/resolvers/userResolvers/queries.ts
@@ -1,15 +1,15 @@
 import { getModelForClass } from "@typegoose/typegoose";
 import { User } from "../../models/User";
 import { queryError } from "../../utils/errors";
-import { QueryResolvers, ResolverTypeWrapper } from "schema/schema";
+import { QueryResolvers, QueryUserArgs } from "schema/schema";
 
 const UserModel = getModelForClass(User);
 
 export const users = async () => {
   try {
     return await UserModel.find();
-  } catch(e) {
-    console.log(e)
+  } catch (e) {
+    console.log(e);
     return queryError;
   }
 };
@@ -22,17 +22,12 @@ export const isUserRegistered: QueryResolvers["isUserRegistered"] = async (
   return false;
 };
 
-// export const user: QueryResolvers["user"] = async (_, { input }) => {
-//   // const {email, fullName, googleID, createdAt, id} = input;
-//   // const foundUsers: ResolverTypeWrapper<User>[] = [];
-//   // // if(id) foundUsers.push(await UserModel.findById(id));
-//   // // if (googleID) return await UserModel.findById({googleID: googleID});
-//   // // if (email) return await UserModel.findById({email: email});
-//   // // // if (fullName) {
-//   // //   return await (UserModel.find({fullName: fullName}))
-//   // // }
-//   // // if (createdAt) {
-//   // //   foundUsers.push(UserModel.find({createdAt: createdAt}))
-//   // // }
-//   return UserModel.find();
-// };
+export const user = async (_: undefined, { input }: QueryUserArgs) => {
+  const { email, fullName, googleID, createdAt, id } = input;
+  if (id) return await UserModel.findById(id);
+  if (googleID) return await UserModel.find({ googleID: googleID });
+  if (email) return await UserModel.find({ email: email });
+  if (fullName) return UserModel.find({ fullName: fullName });
+  if (createdAt) return await UserModel.find({ createdAt: createdAt });
+  return [];
+};
