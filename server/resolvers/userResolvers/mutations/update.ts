6a78e37326a0101d1b959e58030ab2af83ff2287
iamtheef <eftihis.riise@gmail.com>
2020-04-01 23:57:30 +0300

    refactoring

--- server/resolvers/userResolvers/mutations/update.ts
+++ server/resolvers/userResolvers/mutations/update.ts
@@ -16,28 +16,23 @@ export const updateAcc: MutationResolvers["updateAcc"] = async (
     // checks which have been passed and updates accordingly (if the .oldPassword is right)
     const user = await UserModel.findById(id);
 
-    const passwordMatch = await bcrypt.compare(
-      password.oldPassword,
-      user.password.toString()
-    );
-
-    if (passwordMatch) {
-      if (fullName) {
-        await UserModel.findByIdAndUpdate(id, {
-          fullName
-        });
-      }
-
-      if (password.newPassword) {
-        if (password.newPassword === password.confirmPassword) {
-          await UserModel.findByIdAndUpdate(id, {
-            password: bcrypt.hashSync(password.newPassword, 10)
-          });
-        } else
-          return generateUpdateError("PASSWORDS", "Passwords have to match!");
-      }
-    } else
+    if (!(await bcrypt.compare(password.oldPassword, user.password.toString())))
       return generateUpdateError("PASSWORD", "Invalid password, try again.");
+
+    if (fullName) {
+      await UserModel.findByIdAndUpdate(id, {
+        fullName
+      });
+    }
+
+    if (password.newPassword) {
+      if (password.newPassword === password.confirmPassword) {
+        await UserModel.findByIdAndUpdate(id, {
+          password: bcrypt.hashSync(password.newPassword, 10)
+        });
+      } else
+        return generateUpdateError("PASSWORDS", "Passwords have to match!");
+    }
     return { success: true, errors: [] }; // Account Updated
   } catch (e) {
     throw Error(e.message);
--- server/resolvers/userResolvers/queries.ts
+++ server/resolvers/userResolvers/queries.ts
@@ -23,22 +23,23 @@ export const isUserRegistered: GQL_QueryResolvers["isUserRegistered"] = async (
 };
 
 // multiple fields search for users
-// export const userResolver: GQL_QueryResolvers["user"] = async (
-//   _,
-//   { input }
-// ) => {
-//   try {
-//     const { email, fullName, googleID, createdAt, id } = input;
-//     if (id) return await UserModel.findById(id);
-//     if (googleID) return await UserModel.find({ googleID: googleID });
-//     if (email) return await UserModel.find({ email: email });
-//     if (fullName && createdAt) {
-//       return UserModel.find({ fullName: fullName, createdAt: createdAt });
-//     }
-//     if (fullName) return UserModel.find({ fullName: fullName });
-//     if (createdAt) return await UserModel.find({ createdAt: createdAt });
-//     return [] as <User>;
-//   } catch (e) {
-//     throw Error(e.message);
-//   }
-// };
+export const user: GQL_QueryResolvers["user"] = async (_, { input }) => {
+  try {
+    let foundUsers: Array<GQL_User & DB_User> | null = null;
+    const { email, fullName, googleID, createdAt, id } = input;
+    // if (id) foundUsers = await UserModel.findById(id);
+    if (googleID) foundUsers = await UserModel.find({ googleID: googleID });
+    if (email) foundUsers = await UserModel.find({ email: email });
+    if (fullName && createdAt) {
+      foundUsers = await UserModel.find({
+        fullName: fullName,
+        createdAt: createdAt
+      });
+    }
+    if (fullName) foundUsers = await UserModel.find({ fullName: fullName });
+    if (createdAt) foundUsers = await UserModel.find({ createdAt: createdAt });
+    return foundUsers;
+  } catch (e) {
+    throw Error(e.message);
+  }
+};
